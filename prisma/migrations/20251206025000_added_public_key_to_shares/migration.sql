8c2798abfa756355deba706452f7573caf31e852
fegan104 <fegan104@gmail.com>
2025-12-29 12:26:09 -0500

    remove migrations

--- prisma/migrations/20251206025000_added_public_key_to_shares/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `publicKey` to the `shares` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "shares" ADD COLUMN     "publicKey" TEXT NOT NULL;
--- prisma/migrations/20251229040000_drop_file_algorithm/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "encrypted_files" DROP COLUMN "fileAlgorithm";
-
